7b4475ba44acc4b3890475d3061c4da5e1131aae
adegbite mohammed <adedamola13@gmail.com>
2024-04-26 06:49:35 +0100

    Multiplication Assignment

---
+++ MultiplicationTable.js
@@ -0,0 +1,14 @@
+for(let count = 1; count <= 12; count++){
+
+	for(let counter = 1; counter <= 12; counter++){
+
+	process.stdout.write('${counter} * ${count} = ${counter * count} \t')
+
+	}
+
+console.log()
+
+
+
+
+}
